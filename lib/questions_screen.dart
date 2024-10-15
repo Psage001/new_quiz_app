3a2bef08b6a6f3c74e5b5403e8ca5a245d1e272d
Psage01 <markus01.erasmus@gmail.com>
2024-10-15 11:48:55 +0200

    adding answer buttons

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,19 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:new_quiz_app/answer_button.dart';
 
-class QuestionsScreen extends StatefulWidget{
- const QuestionsScreen({super.key});
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
 
   @override
   State<QuestionsScreen> createState() {
     return _QuestionScreenState();
   }
-  
-
 }
 
-class _QuestionScreenState extends State<QuestionsScreen>{
+class _QuestionScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('data');
+    return  SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+           const Text(
+            'The questions...',
+            style: TextStyle(color: Colors.white),
+          ),
+           const SizedBox(
+            height: 30,
+          ),
+         AnswerButton(
+          onTap:(){},
+          answerText: 'Text',
+          
+          ),
+         
+         AnswerButton(
+          onTap:(){},
+          answerText: 'Text',
+          ),
+         
+         AnswerButton(
+          onTap:(){},
+          answerText: 'Text',
+          ),
+         
+        ],
+      ),
+    );
   }
 }
